2a1bf87d952d6b60f6d5a261f92a0a08a1dff3d2
certificationSurya - Pc <certification.surya@gmail.com>
2024-06-08 19:52:26 +0545

    fix: seen issues, gotta add conversation field

--- controllers/temp/userController.js
+++ controllers/temp/userController.js
@@ -1,21 +1,41 @@
 import User from "../../config/models/userModel.js";
+import {Conversation} from "../../config/models/chatModel.js";
 
 const userController = {
 	get: async (req, res) => {
-        
-		const { userId } = req.params;
+		const {userId} = req.params;
 
 		try {
-            const users = await User.find(
-                { _id: { $ne: userId } }, // no currentUser
-                { password: 0 } // noPassword field
-              );
+			const users = await User.find(
+				{_id: {$ne: userId}}, // no currentUser
+				{password: 0} // noPassword field
+			);
 
-			console.log(users);
-            return res.status(200).json({message: "retrieved users successfully", users});
+			const conversationUserIsIn = await Conversation.find({
+				$or: [{senderId: userId}, {receiverId: userId}],
+			});
+
+                  let chattedUserInfo = [];
+			if (conversationUserIsIn) {
+				const userSet = new Set();
+				conversationUserIsIn.forEach((convo) => {
+					if (convo.senderId != userId) {
+						userSet.add(convo.senderId);
+					} else {
+						userSet.add(convo.receiverId);
+					}
+				});
+				const chattedUserId = Array.from(userSet);
+
+                        chattedUserInfo = await User.find({ _id: { $in: chattedUserId } }, 'username email');
+			}
+
+			return res
+				.status(200)
+				.json({message: "retrieved users successfully", users, chattedUsers: chattedUserInfo});
 		} catch (err) {
-            console.error(err);
-            return res.status(500).json({message: "Unable to retrieve users"});
+			console.error(err);
+			return res.status(500).json({message: "Unable to retrieve users"});
 		}
 	},
 };
