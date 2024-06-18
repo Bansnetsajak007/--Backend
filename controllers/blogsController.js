4832a7f28dda410c16b93b82e7d1658deedcdde3
certificationSurya - Pc <certification.surya@gmail.com>
2024-06-18 17:23:33 +0545

    fix: blog route and controller

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -14,12 +14,12 @@ const blogsController = {
 
     createPost: async(req, res) => {
         try {
-            const { title, body } = req.body;
-            const newPost = new Blogs({ title, body });
+            const { title, body, createdBy, userId } = req.body;
+            const newPost = new Blogs({ title, body, createdBy, userId });
             await newPost.save();
-            res.status(201).json(newPost);
+            res.status(200).json({message: "Your post has been created"});
           } catch (err) {
-            res.status(500).json({ error: err.message });
+            res.status(500).json({ message: "Internal Server Error Occured !" });
           }
     },
 
@@ -27,17 +27,18 @@ const blogsController = {
         try {
             const post = await Blogs.findById(req.params.id);
             if (!post) return res.status(404).json({ error: 'Post not found' });
-            res.json(post);
+            res.status(200).json({data: post});
           } catch (err) {
-            res.status(500).json({ error: err.message });
+            res.status(500).json({ message: "Internal Server Error Occured !" });
           }
     },
 
     updatePost: async(req, res) => {
+      const id = req.params.id;
         try {
             const { title, body } = req.body;
             const post = await Blogs.findByIdAndUpdate(
-              req.params.id,
+              id,
               { title, body },
               { new: true }
             );
